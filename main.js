484a5fb0d6547f4ff9e86c0947e6b65ddb5bdb56
H3790A <117698796+H3790A@users.noreply.github.com>
2023-07-06 17:44:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX = "";
+noseY = "";
+
+function preload() {
+clown_image = loadImage("https://i.postimg.cc/LXQcBd4h/download-1.jpg");
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(noseX, noseY, 20);
+    image(clown_image,noseX,noseY,30,30);
+}
+function take_snapshot() {
+    save("student.png");
+}
+function modelLoaded() {
+    console.log("poseNet is loaded");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose- x =" + results[0].pose.nose.x);
+        console.log("nose- y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+    }
+}
